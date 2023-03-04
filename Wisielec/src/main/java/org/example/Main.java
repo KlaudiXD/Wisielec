5213ee1dbf91b57599debf4723a26f16acf226be
Klaudyna2603 <klaudyna.fialek@gmail.com>
2023-03-04 14:40:19 +0100

    Rozpoczęcie projektu

---
+++ Wisielec/src/main/java/org/example/Main.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    Scanner scanner= new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String [] tab={"pies","kot","bocian","krowa","chomik"};
+       // random();
+        String one= tab[random()];
+        System.out.println(one);
+        int max=10;
+        int error=0;
+        char [] arr= one.toCharArray();
+        char [] arrTwo= new char[arr.length];
+        System.out.println("To jest gra wisielec :)");
+        System.out.println("Zgadnij wylosowane słowo");
+        fill(arrTwo);
+        print(arrTwo);
+
+
+
+    }
+    public static int random (){
+        int random= (int) (Math.random()*5);
+
+        return random;
+
+    }
+    public static void fill (char [] cha){
+        for (int i = 0; i < cha.length; i++) {
+            cha[i]= '_';
+
+        }
+
+    }
+    public static void print (char [] cha){
+        for (int i = 0; i < cha.length; i++) {
+            System.out.print(cha[i]);
+
+        }
+
+    }
+
+}
